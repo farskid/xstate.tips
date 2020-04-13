9503ac732147fac03fb6eb72cbce73146941b25e
Taylor Thompson <jamestthompson3@gmail.com>
2020-04-13 17:09:46 +0300

    Adds rss feed support

--- scripts/compile-tips.js
+++ scripts/compile-tips.js
@@ -4,26 +4,28 @@ const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
 
+const HOME_URL = "https://xstate.tips/";
 const DATA_DIR = path.join(process.cwd(), "data");
 const MARKDOWN_DIR = path.join(DATA_DIR, "tips");
 const COMPILE_FILE = path.join(DATA_DIR, "_tips.json");
+const RSS_FILE = path.join(process.cwd(), "static/feed.xml");
 const remarkableConfig = {
   html: false,
   xhtmlOut: true,
   breaks: false,
   typographer: true,
-  linkify: true,
+  linkify: true
 };
 
 const collectMarkdowns = () => {
   return fs.readdirSync(MARKDOWN_DIR);
 };
 
-const saveTips = (tipsJson) => {
+const saveTips = tipsJson => {
   fs.writeFileSync(COMPILE_FILE, JSON.stringify(tipsJson));
 };
 
-const slugify = (title) =>
+const slugify = title =>
   title
     .toLowerCase()
     .split(" ")
@@ -37,23 +39,24 @@ const slugify = (title) =>
     .split(/[。？！，、；：“”【】（）〔〕［］﹃﹄“ ”‘’﹁﹂—…－～《》〈〉「」]/)
     .join("");
 
+const markdownFiles = collectMarkdowns();
+const markdownContents = markdownFiles.map(file => ({
+  name: file,
+  md: fs.readFileSync(path.join(MARKDOWN_DIR, file)).toString()
+}));
+
+const parser = new MetaRemarkable("full", remarkableConfig);
+
 // Main
 (function main() {
-  const markdownFiles = collectMarkdowns();
-  const markdownContents = markdownFiles.map((file) => ({
-    name: file,
-    md: fs.readFileSync(path.join(MARKDOWN_DIR, file)).toString(),
-  }));
-
-  const parser = new MetaRemarkable("full", remarkableConfig);
-
-  const compiledContents = markdownContents.map((file) => {
+  const compiledContents = markdownContents.map(file => {
     const { meta, html } = parser.render(file.md);
+    const slug = slugify(meta.title);
 
     return {
       ...meta,
       html,
-      slug: slugify(meta.title),
+      slug
     };
   });
 
@@ -66,4 +69,39 @@ const slugify = (title) =>
     )
   );
   saveTips(compiledContents);
+  generateFeed(compiledContents);
 })();
+
+function generateFeed(tips) {
+  let feedXML = `<?xml version="1.0" encoding="UTF-8"?>
+      <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+      <channel>
+      <title>XState.tips</title>
+      <description>Helping you take finite state machines and Statecharts into real world development.</description>
+      <link>${HOME_URL}</link>
+      <atom:link href="${HOME_URL}feed.xml" rel="self" type="application/rss+xml" />`;
+
+  const pubDateFallback = new Date();
+
+  for (const tip of tips) {
+    feedXML += `\n<item><title>${tip.title}</title><description>${escapeHtml(
+      tip.html
+    )}</description><pubDate>${tip.pubDate ||
+      pubDateFallback.toLocaleString()}</pubDate><link>${HOME_URL}${
+      tip.slug
+    }</link><guid isPermaLink="true">${HOME_URL}${tip.slug}</guid></item>`;
+  }
+
+  feedXML += "\n</channel>\n</rss>";
+  fs.writeFileSync(RSS_FILE, feedXML);
+  console.log(`📰\x1b[35mSuccessfully compiled RSS feed!\x1b[0m`);
+}
+
+function escapeHtml(raw) {
+  return raw
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
