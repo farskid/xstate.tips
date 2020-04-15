73c268735c5bcc357367a70dcae5e8bcfa273960
Farzad YZ <farskid@gmail.com>
2020-04-15 15:10:27 +0300

    Responsive menu

---
+++ src/components/Hamburger.svelte
@@ -0,0 +1,106 @@
+<script>
+  export let isActive = false;
+
+  function getHamburgerClassname(isActive) {
+    return isActive ? "hamburger is-active" : "hamburger";
+  }
+</script>
+
+<style>
+  .hamburger {
+    padding: 0 0;
+    display: inline-block;
+    cursor: pointer;
+    transition-property: opacity, filter;
+    transition-duration: 0.15s;
+    transition-timing-function: linear;
+    font: inherit;
+    color: inherit;
+    text-transform: none;
+    background-color: transparent;
+    border: 0;
+    margin: 0;
+    overflow: visible;
+  }
+  .hamburger.is-active .hamburger-inner,
+  .hamburger.is-active .hamburger-inner::before,
+  .hamburger.is-active .hamburger-inner::after {
+    background-color: var(--text-primary);
+  }
+
+  .hamburger-box {
+    width: 20px;
+    height: 14px;
+    display: inline-block;
+    position: relative;
+  }
+
+  .hamburger-inner {
+    display: block;
+    top: 50%;
+    margin-top: -1px;
+  }
+  .hamburger-inner,
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    width: 20px;
+    height: 2px;
+    background-color: var(--text-primary);
+    border-radius: 0;
+    position: absolute;
+    transition-property: transform;
+    transition-duration: 0.15s;
+    transition-timing-function: ease;
+  }
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    content: "";
+    display: block;
+  }
+  .hamburger-inner::before {
+    top: -6px;
+  }
+  .hamburger-inner::after {
+    bottom: -6px;
+  }
+
+  /*
+   * Spring
+   */
+  .hamburger .hamburger-inner {
+    top: 1px;
+    transition: background-color 0s 0.13s linear;
+  }
+  .hamburger .hamburger-inner::before {
+    top: 6px;
+    transition: top 0.1s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1),
+      transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+  .hamburger .hamburger-inner::after {
+    top: 12px;
+    transition: top 0.2s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1),
+      transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+  .hamburger.is-active .hamburger-inner {
+    transition-delay: 0.22s;
+    background-color: transparent !important;
+  }
+  .hamburger.is-active .hamburger-inner::before {
+    top: 0;
+    transition: top 0.1s 0.15s cubic-bezier(0.33333, 0, 0.66667, 0.33333),
+      transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 6px, 0) rotate(45deg);
+  }
+  .hamburger.is-active .hamburger-inner::after {
+    top: 0;
+    transition: top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333),
+      transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 6px, 0) rotate(-45deg);
+  }
+</style>
+
+<span class={getHamburgerClassname(isActive)}>
+  <span class="hamburger-box">
+    <span class="hamburger-inner" />
+  </span>
+</span>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,8 +3,10 @@
   import tips from "../../data/_tips.json";
   import metadata from "../../data/metadata.json";
   import Search from "./Search.svelte";
+  import Hamburger from "./Hamburger.svelte";
   export let segment;
   let filteredTips = tips;
+  let sidebarShownOnMobile = false;
 
   function isPageActive(segment, tip, index) {
     return (segment === undefined && index === 0) || segment === tip.slug;
@@ -19,6 +21,22 @@
 
   function getAuthorUrl(author) {
     return author.twitter || author.github;
+  }
+
+  function getSidebarClassname(sidebarShownOnMobile) {
+    return sidebarShownOnMobile ? "shown" : undefined;
+  }
+
+  function getFloatingButtonText(sidebarShownOnMobile) {
+    return sidebarShownOnMobile ? "Close" : "All Tips";
+  }
+
+  function toggleSidebar() {
+    sidebarShownOnMobile = !sidebarShownOnMobile;
+  }
+
+  function closeSidebar() {
+    sidebarShownOnMobile = false;
   }
 </script>
 
@@ -38,6 +56,8 @@
     flex-direction: column;
     align-items: stretch;
     padding: 1em 2em;
+    transition: 0.2s ease-out;
+    background-color: var(--white);
   }
 
   aside :global(form),
@@ -86,15 +106,59 @@
   .heart {
     color: var(--red);
   }
+
+  .floating-button {
+    position: fixed;
+    bottom: 0.5em;
+    right: 0.5em;
+    z-index: 1;
+    background-color: var(--white);
+    color: var(--text-primary);
+    font-size: 1rem;
+    border: 1px solid var(--border);
+    display: none;
+    padding: 0.5em;
+    text-align: center;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+  }
+
+  /* Responsiveness */
+  @media screen and (max-width: 768px) {
+    aside {
+      z-index: 1;
+      position: fixed;
+      top: var(--site-header-height);
+      left: 0;
+      right: 0;
+      bottom: 0;
+      transform: translateY(100%);
+    }
+
+    aside.shown {
+      transform: translateY(0);
+    }
+
+    .floating-button {
+      display: block;
+    }
+
+    aside,
+    .main-content {
+      padding: 0.5em;
+    }
+  }
 </style>
 
 <div class="site-container">
-  <aside>
+  <aside class={getSidebarClassname(sidebarShownOnMobile)} id="navigation">
     <Search onChange={filterTips} />
     <ul>
       {#each filteredTips as tip, index}
         <li>
           <a
+            on:click={closeSidebar}
             href={tip.slug}
             aria-current={isPageActive(segment, tip, index) ? 'page' : undefined}>
             {tip.title}
@@ -105,7 +169,7 @@
     <footer>
       <small>
         Made with
-        <span class="heart">&#10084;</span>
+        <span class="heart" title="Love">&#10084;</span>
         <br />
         By
         {#each metadata.authors as author, index}
@@ -120,6 +184,14 @@
     <div class="main-content">
       <!-- Required for sapper to inject page content -->
       <slot />
+      <button
+        class="floating-button"
+        on:click={toggleSidebar}
+        aria-label="Menu"
+        type="button"
+        aria-controls="navigation">
+        <Hamburger isActive={sidebarShownOnMobile} />
+      </button>
     </div>
   </main>
 </div>
