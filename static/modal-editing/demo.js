e495a270e747461dbf7fb75d3840eeae67485c72
Taylor Thompson <jamestthompson3@gmail.com>
2020-04-26 00:38:50 +0300

    Properly sets up serialized actions

--- static/modal-editing/demo.js
+++ static/modal-editing/demo.js
@@ -6,104 +6,104 @@ const cursor = document.getElementById("cursor");
 const terminalText = terminal.getElementsByTagName("span")[0];
 const showmode = document.getElementById("showmode");
 
-function applyInsertModeStyles() {
-  console.log("NOT SERIALIZED, WILL GET CALLED");
-  showmode.textContent = "-- INSERT --";
-  cursor.style.width = "2.5px";
-}
-
 // MACHINE SETUP
 // *****************************
 //
 const { Machine, interpret, assign } = XState;
 
-const modeMachine = Machine({
-  id: "input-mode",
-  initial: "normal",
-  context: {
-    cursorPointer: undefined,
-    textLength: undefined
-  },
-  states: {
-    normal: {
-      entry: "applyNormalModeStyles",
-      on: {
-        INSERT: "insert",
-        VISUAL: "visual",
-        REPLACE: "replace",
-        CURSOR_LEFT: {
-          target: ".",
-          actions: "moveLeft",
-          internal: true
-        },
-        CURSOR_RIGHT: {
-          target: ".",
-          actions: "moveRight",
-          internal: true
+const modeMachine = Machine(
+  {
+    id: "input-mode",
+    initial: "normal",
+    context: {
+      cursorPointer: undefined,
+      textContent: "",
+      textLength: undefined
+    },
+    states: {
+      normal: {
+        entry: "applyNormalModeStyles",
+        on: {
+          INSERT: "insert",
+          VISUAL: "visual",
+          REPLACE: "replace",
+          CURSOR_LEFT: {
+            target: ".",
+            actions: "moveLeft",
+            internal: true
+          },
+          CURSOR_RIGHT: {
+            target: ".",
+            actions: "moveRight",
+            internal: true
+          }
         }
-      }
-    },
-    visual: {
-      entry: "applyVisualModeStyles",
-      on: { NORMAL: "normal" }
-    },
-    insert: {
-      entry: applyInsertModeStyles,
-      on: {
-        NORMAL: "normal",
-        UPDATE_TEXT: {
-          target: ".",
-          actions: "updateText"
+      },
+      visual: {
+        entry: "applyVisualModeStyles",
+        on: { NORMAL: "normal" }
+      },
+      insert: {
+        entry: "applyInsertModeStyles",
+        on: {
+          NORMAL: "normal",
+          UPDATE_TEXT: {
+            target: ".",
+            actions: "updateText"
+          }
         }
+      },
+      replace: {
+        entry: "applyReplaceModeStyles",
+        on: { NORMAL: "normal" }
       }
-    },
-    replace: {
-      entry: "applyReplaceModeStyles",
-      on: { NORMAL: "normal" }
     }
   },
-  actions: {
-    updateText: (ctx, e) => {
-      console.log("UPDATE", e);
-      terminalText.textContent = e.value;
-      assign({
-        textContent: e.value
-      });
-    },
-    moveLeft: ctx => {
-      const cursorPointer = ctx.cursorPointer - 1;
-      cursor.style.left = `${(ctx.textLength - cursorPointer) * -7}px`;
-      assign({
-        cursorPointer
-      });
-    },
-    moveRight: ctx => {
-      const cursorPointer = ctx.cursorPointer + 1;
-      const calculatedPosition = (ctx.textLength - cursorPointer) * 7;
-      const lineEnd = calculatedPosition > 0;
-      cursor.style.left = `${lineEnd ? 0 : calculatedPosition}px`;
-      assign({
-        cursorPointer
-      });
-    },
-    // applyInsertModeStyles: () => {
-    //   console.log("APPLYING INSERT STYLES");
-    //   showmode.textContent = "-- INSERT --";
-    //   cursor.style.width = "2.5px";
-    // },
-    applyReplaceModeStyles: () => {
-      showmode.textContent = "-- REPLACE --";
-    },
-    applyNormalModeStyles: () => {
-      console.log("SERIALIZED, DOES NOT GET CALLED");
-      showmode.textContent = "";
-      cursor.style.width = "7px";
-    },
-    applyVisualModeStyles: () => {
-      showmode.textContent = "-- VISUAL --";
+  {
+    actions: {
+      updateText: (ctx, e) => {
+        terminalText.textContent = e.data;
+        assign({
+          textContent: e.data,
+          textLength: e.data.length
+        });
+      },
+      moveLeft: ({ textLength, cursorPointer: ctxCursor }) => {
+        const cursorPointer = ctxCursor ? ctxCursor - 1 : textLength;
+        console.log({ textLength, cursorPointer });
+        const calculatedPosition = (textLength - cursorPointer) * -7;
+        console.log(calculatedPosition);
+        cursor.style.left = `${calculatedPosition}px`;
+        assign({
+          cursorPointer
+        });
+      },
+      moveRight: ctx => {
+        const cursorPointer = ctx.cursorPointer + 1;
+        const calculatedPosition = (ctx.textLength - cursorPointer) * 7;
+        const lineEnd = calculatedPosition > 0;
+        cursor.style.left = `${lineEnd ? 0 : calculatedPosition}px`;
+        assign({
+          cursorPointer
+        });
+      },
+      applyInsertModeStyles: () => {
+        showmode.textContent = "-- INSERT --";
+        cursor.style.width = "2.5px";
+      },
+      applyReplaceModeStyles: () => {
+        showmode.textContent = "-- REPLACE --";
+      },
+      applyNormalModeStyles: () => {
+        showmode.textContent = "";
+        cursor.style.width = "7px";
+      },
+      applyVisualModeStyles: () => {
+        showmode.textContent = "-- VISUAL --";
+      }
     }
   }
-});
+);
 
 // DEBUG UTILS
 // *********************************
@@ -140,9 +140,9 @@ setInterval(() => {
 input.addEventListener(
   "input",
   e => {
-    console.log(state);
-    modalService.send({ type: "UPDATE_TEXT", data: e.target.value });
-    if (state !== "insert") {
+    if (state === "insert") {
+      modalService.send({ type: "UPDATE_TEXT", data: e.target.value });
+    } else {
       // Do this so we don't have a delay between typing the input and showing the value.
       // Also so that we don't add things to the input that get suddenly appened after entering insert mode
       e.target.value = e.target.value.slice(0, e.target.value.length - 1);
@@ -157,7 +157,7 @@ function changeModes(e) {
   if (state === "normal") {
     handleNormalModeInput(e.key);
   } else {
-    if (e.key === "Esc") modalService.send("NORMAL");
+    if (e.key === "Escape") modalService.send("NORMAL");
   }
 }
 
