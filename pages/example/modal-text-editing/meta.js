39aa5da1f2f84d80ce28f4d080402f5fdb4991e5
taylor <jamestthompson3@gmail.com>
2020-10-28 21:57:32 +0200

    [WIP] Drafting modal editing tip

---
+++ pages/example/modal-text-editing/meta.js
@@ -0,0 +1,10 @@
+const meta = {
+  title: "Modal text editing",
+  author: "taylor thompson",
+  description:
+    "Neovim is my favorite editor, partly due to the modal editing approach. Learn how to use state machines to implement modal editing basics",
+  pubDate: "Oct 28 2020",
+  url: "/modal-text-editing",
+};
+
+module.exports.meta = meta;
